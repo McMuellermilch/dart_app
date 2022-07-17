49c38a8ac4298587529cb66d39891e6d59889e11
Florian Mueller <mueller.florian99@outlook.com>
2022-07-17 15:01:49 +0200

    Initial Commit

---
+++ dart-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  dart-app
+//
+//  Created by Florian Müller on 17.07.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ dart-app/dart_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  dart_appApp.swift
+//  dart-app
+//
+//  Created by Florian Müller on 17.07.22.
+//
+
+import SwiftUI
+
+@main
+struct dart_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
